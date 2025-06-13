4565ddcf9bf3b0af4956382fc850f9af6550b48a
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-06-13 16:42:32 +0300

    Update tmp.blade.php
    1. Added default style values ​​for 'html, body and h' tags.
    2. Visual improvements and fixes have been made.

--- tmp.blade.php
+++ tmp.blade.php
@@ -1,27 +1,38 @@
 <!doctype html>
-<html lang="tr" class="scroll-smooth">
+<html lang="tr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sanat Teorisi</title>
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-        <style type="text/tailwindcss">
-          @theme {
-            --breakpoint-3xl: 120rem; /* 1920px */
-            --breakpoint-4xl: 160rem; /* 2560px */
-            --breakpoint-5xl: 240rem; /* 3840px */
-          }
-        </style>
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+            <style type="text/tailwindcss">
+                @theme {
+                  --breakpoint-3xl: 120rem; /* 1920px */
+                  --breakpoint-4xl: 160rem; /* 2560px */
+                  --breakpoint-5xl: 240rem; /* 3840px */
+                }
+                @layer{
+                    html {@apply scroll-smooth;}
+                    body {@apply bg-linear-to-b from-gray-700 from-20% to-white to-80%;}
+                    h1 {@apply text-2xl lg:text-3xl font-medium;}
+                    h2 {@apply text-xl lg:text-2xl font-bold;}
+                    h3 {@apply font-medium;}
+                    h5 {@apply lg:text-lg font-bold;}
+                    h6 {@apply font-bold;}
+                }
+            </style>
+        @endif
     </head>
-    <body class="bg-linear-to-b from-gray-700 from-20% to-white to-80%" id="body">
+    <body id="body">
         <div class="3xl:w-[1905px] justify-self-center" id="container">
 
-            <header class="grid grid-cols-2 md:grid-cols-3 sticky top-0 bg-linear-to-b from-black to-transparent backdrop-blur-[2px] drop-shadow-md/50 text-white p-5 lg:p-10" id="header">
-                <div class="text-lg lg:text-2xl font-medium">
-                    <h1><a href="/">Sanat Teorisi</a></h1>
-                </div>
+            <header class="grid grid-cols-2 md:grid-cols-3 p-5 lg:p-10 sticky top-0 bg-linear-to-b from-black to-transparent backdrop-blur-[2px] drop-shadow-md/50 text-white z-50" id="header">
+                <h1><a href="/">Sanat Teorisi</a></h1>
                 <nav class="hidden md:flex gap-x-4 lg:gap-x-8 justify-start self-center lg:text-xl font-medium text-gray-400 uppercase">
-                    <a href="javascript:;" class="hover:text-orange-400 transition duration-300">Galeri</a>
+                    <a href="javascript:;" class="hover:text-orange-400 transition duration-300 ease-in-out">Galeri</a>
                     <a href="javascript:;" class="hover:text-orange-400 transition duration-300">Makale</a>
                     <a href="javascript:;" class="hover:text-orange-400 transition duration-300">Şiir</a>
                     <a href="javascript:;" class="hover:text-orange-400 transition duration-300">Sözlük</a>
@@ -66,59 +77,59 @@
                 -->
             </header>
 
-            <section class="grid grid-cols-1 gap-4 px-5 lg:px-10 bg-gray-400" id="hero">
+            <section class="grid grid-cols-1 px-5 lg:px-10 bg-gray-400" id="hero">
                 <div class="h-100">
-                    Hero
+                    <h2>Hero Section</h2>
                 </div>
             </section>
 
             <main class="bg-gray-300" id="main">
-                <div class="grid grid-cols-1 lg:grid-cols-2 bg-gray-300 text-slate-200">
+                <div class="grid grid-cols-1 lg:grid-cols-2">
                     <div class="p-5 lg:p-10 text-gray-600 bg-gray-50" id="best-articles">
-                        <h2 class="flex p-3 text-2xl font-bold">EN İYİ MAKALELER</h2>
+                        <h2><span class="p-3">EN İYİ MAKALELER</span></h2>
                         <p class="p-3 border-b border-gray-400">Makaleler bölümündeki en iyi 5 içerik.</p>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Eser Analiz Yöntemleri</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Eser Analiz Yöntemleri</a></h3>
                             <p>Sanat olgusunun varlığını kavramanın en doğru yolu, sanat eserini çözümlemekte yatmaktadır. Bu konuy...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Türk Resminde Kurtuluş Savaşı Teması</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Türk Resminde Kurtuluş Savaşı Teması</a></h3>
                             <p>Sanatın toplumsal yapılara, bağlı gelişmesiyle, sanatçının yaratımını politik, ekonomik, kültürel şa...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Altın Oran</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Altın Oran</a></h3>
                             <p>“Altın oran kavramı ve bu kavramın gizemi nedir?” diye düşündüğünüz olmuştur. Belki de bu kavramı il...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Sokrates ve Felsefesi</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Sokrates ve Felsefesi</a></h3>
                             <p>M.Ö. 469-399 yılları arasında yaşamış olan ünlü Yunanlı düşünür. Platon’un hocası olan Sokrates, yaz...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Modernizm ve Sanat Akımlarından Notlar</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Modernizm ve Sanat Akımlarından Notlar</a></h3>
                             <p>1750’lerden 1890’lara kadar süren ilk sanayi devrimi ardından, 2. Sanayi Devri 1896’larda başlayıp 1...</p>
                         </article>
                     </div>
                     <div class="p-5 lg:p-10 text-gray-600" id="last-articles">
-                        <h2 class="flex p-3 text-2xl font-bold">SON 5 MAKALE</h2>
+                        <h2><span class="p-3">SON 5 MAKALE</span></h2>
                         <p class="p-3 border-b border-gray-400">Makaleler bölümündeki son 5 içerik.</p>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Sürreal Resimde Kadın İmgesi</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Sürreal Resimde Kadın İmgesi</a></h3>
                             <p>Sürrealizm, sanat ve yaşam alanında radikal başkaldırı ve kural tanımaz tavrın 20 yüz yılın ilk çeyr...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Büyük Masturbator</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Büyük Masturbator</a></h3>
                             <p>Büyük Masturbator, Salvador Dalinin ikonik bir eseri ve ressamın sürrealist dönemine ait olarak kabu...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Ölümün Kitsch Bir İmge Örneği Olarak Günümüz İran’ında Üretilen Mezar Taşları</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Ölümün Kitsch Bir İmge Örneği Olarak Günümüz İran’ında Üretilen Mezar Taşları</a></h3>
                             <p>Farklı çağlarda ölümle ilgili üretilen imgeler, metafiziğe nasıl bakıldığını dışa vurmaktadır. Kitsc...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Sanatta Bedenin Uyku Hali</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Sanatta Bedenin Uyku Hali</a></h3>
                             <p>Beden, canlının maddesel kanıtıdır. Bedeni betimlemek çağlar boyunca sanatın en önemli sorunsalların...</p>
                         </article>
                         <article class="block p-3 hover:bg-gray-200 transition duration-300">
-                            <h3 class="text-orange-500 font-medium"><a href="javascript:;" class="hover:underline">Orhan Pamuk’un Beyaz Kale Romanında Ayna Oyunları</a></h3>
+                            <h3><a href="javascript:;" class="text-orange-500 hover:underline">Orhan Pamuk’un Beyaz Kale Romanında Ayna Oyunları</a></h3>
                             <p>Genelde ayna varlıkların görüntüsünü yansıtan ya da süs olarak kullanılan eşyalardan biri olarak düş...</p>
                         </article>
                     </div>
@@ -150,43 +161,58 @@
                         <img src="https://images.unsplash.com/photo-1526400473556-aac12354f3db" alt=".">
                     </div>
                 </div>
+
+                <div class="grid py-2 px-10 bg-gray-200 rounded-xl" id="article-of-the-week">
+                    <div class="bg-gray-100 px-5">
+                        <h2>
+                            <label for="menu-collapse" class="cursor-pointer block py-5">Haftanın Yazısı: Dada Manifestosu</label>
+                        </h2>
+                    </div>
+                    <input type="checkbox" class="hidden peer" id="menu-collapse">
+                    <div class="hidden peer-checked:block px-5 bg-white">
+                        <h3>Dada Manifestosu</h3>
+                        <p>Gazetecileri beklenmedik bir dünyanın kapısına getiren bir sözcüğün – DADA – büyüsü, bizim için hiçbir önem taşımıyor..</p>
+                        <p>Yazar: Tristan Tzara</p>
+                    </div>
+                </div>
+
             </main>
 
-            <div class="w-full" id="bottom">
+            <section class="w-full" id="bottom">
                 <div class="grid grid-cols-2 lg:grid-cols-5 gap-4 p-5 pb-1 lg:px-10 bg-gray-300 text-gray-700" id="bottom-up">
                     <div class="hidden lg:block w-10 -rotate-180 font-black text-4xl text-gray-400 select-none" style="writing-mode:vertical-lr">Sanat Teorisi</div>
                     <div class="px-5 text-sm">
-                        <h3 class="lg:text-lg font-bold">Makaleler</h3>
+                        <h5>Makaleler</h5>
                         <p><a href="javascript:;" class="hover:underline">Popüler makaleler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok okunan makaleler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok yorum alan makaleler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Yeni eklenen makaleler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Alfabetik sıraya göre makaleler</a></p>
-                        <p><a href="javascript:;" class="hover:underline">Toplam makale sayısı (390)</a></p>
+                        <p><a href="javascript:;" class="hover:underline">Toplam makale (390)</a></p>
                     </div>
                     <div class="px-5 text-sm">
-                        <h3 class="lg:text-lg font-bold">Şiirler</h3>
+                        <h5>Şiirler</h5>
                         <p><a href="javascript:;" class="hover:underline">Popüler şiirler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok okunan şiirler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok yorum alan şiirler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Yeni eklenen şiirler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Alfabetik sıraya göre şiirler</a></p>
-                        <p><a href="javascript:;" class="hover:underline">Toplam şiir sayısı (2578)</a></p>
+                        <p><a href="javascript:;" class="hover:underline">Toplam şiir (2578)</a></p>
                     </div>
                     <div class="px-5 text-sm">
-                        <h3 class="lg:text-lg font-bold">Galeri</h3>
+                        <h5>Galeri</h5>
                         <p><a href="javascript:;" class="hover:underline">Popüler resimler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok izlenen resimler</a></p>
                         <p><a href="javascript:;" class="hover:underline">En çok yorum alan resimler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Yeni eklenen resimler</a></p>
                         <p><a href="javascript:;" class="hover:underline">Alfabetik sıraya göre resimler</a></p>
-                        <p><a href="javascript:;" class="hover:underline">Toplam resim sayısı (1010)</a></p>
+                        <p><a href="javascript:;" class="hover:underline">Toplam resim (1010)</a></p>
                     </div>
                     <div class="px-5 text-sm">
-                        <h3 class="lg:text-lg font-bold">Üye/Ziyaret</h3>
+                        <h5>Üye/Ziyaret</h5>
                         <p><a href="javascript:;" class="hover:underline">Aktif üye sayısı (1)</a></p>
                         <p><a href="javascript:;" class="hover:underline">Aktif ziyaretçiler (0)</a></p>
-                        <p><a href="javascript:;" class="hover:underline">Kayıtlı üye sayısı (1171)</a></p>
+                        <p><a href="javascript:;" class="hover:underline">Kayıtlı üye (1171)</a></p>
                         <p><a href="javascript:;" class="hover:underline">Yeni Üyelik</a></p>
                     </div>
                     <div class="col-2 col-start-2 lg:col-5 lg:col-start-5 flex justify-end">
@@ -202,13 +228,13 @@
                 </div>
                 <div class="flex justify-end space-x-8 p-5 lg:px-10 bg-gray-400 text-gray-700" id="bottom-down">
                     <div class="text-sm">
-                        <h3 class="font-bold">Proje ve Referanslar</h3>
+                        <h6>Proje ve Referanslar</h6>
                         <p><a href="javascript:;" class="hover:underline">sanatteorisi.com</a></p>
                         <p><a href="javascript:;" class="hover:underline">sanatteorisi.net</a></p>
                         <p><a href="javascript:;" class="hover:underline">mustafabilgi.com</a></p>
                     </div>
                     <div class="text-sm">
-                        <h3 class="font-bold">Sanat Teorisi</a></h3>
+                        <h6>Sanat Teorisi</h6>
                         <p><a href="javascript:;" class="hover:underline">Hakkımızda</a></p>
                         <p><a href="javascript:;" class="hover:underline">Gizlilik İlkesi</a></p>
                         <p><a href="javascript:;" class="hover:underline">Reklam</a></p>
@@ -216,14 +242,14 @@
                         <p><a href="javascript:;" class="hover:underline">Site Haritası</a></p>
                     </div>
                     <div class="text-sm">
-                        <h3 class="font-bold">Yardım</h3>
+                        <h6>Yardım</h6>
                         <p><a href="javascript:;" class="hover:underline">Kurallar</a></p>
                         <p><a href="javascript:;" class="hover:underline">Kullanım Klavuzu</a></p>
                         <p><a href="javascript:;" class="hover:underline">Sık Sorulan Sorular</a></p>
                         <p><a href="javascript:;" class="hover:underline">Tüm Yardım Konuları</a></p>
                     </div>
                 </div>
-            </div>
+            </section>
 
             <footer class="p-5 lg:p-10 bg-gray-800 text-gray-500 text-right text-sm" id="footer">
                 <p>Resimlerin izin alınmadan kopyalanması ve kullanılması <a href="javascript:;" class="text-gray-300 hover:underline">5846 sayılı fikir ve sanat eserleri kanunu</a>na göre suçtur.</p>
@@ -242,4 +268,11 @@
 *
 * SVG ICONS PACK
 * https://heroicons.com/outline
+*
+* h1: Only Logo
+* h2: UI & section title (example: 'Best Articles', 'Top 10 Topics' etc...)
+* h3: Content title
+* h4: n/a
+* h5: UI bottom title
+* h6: UI bottom & footer title
 -->
