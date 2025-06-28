4eafb81968a9f1f8a9162e50ba28d7ca162e5369
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-06-28 16:47:15 +0300

    Create slider.js

---
+++ public/js/slider.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const TRACK = document.getElementById('sliderTrack');
+    const BUTTONS = document.querySelectorAll('#navButtons button');
+    const SLIDER = document.getElementById('slider');
+    const TOTAL_SLIDES = 4;     // 20 images / 5 per view
+    const SLIDE_TIME = 3000;    // 3sn
+    let currentIndex = 0;
+    let interval = null;
+
+    function goToSlide(index) {
+        TRACK.style.transform = `translateX(-${index * 100}%)`;
+        BUTTONS.forEach(btn => btn.classList.remove('bg-red-500'));
+        BUTTONS[index].classList.add('bg-red-500');
+        currentIndex = index;
+    }
+
+    function startAutoSlide() {
+        stopAutoSlide(); // önce varsa temizle
+        interval = setInterval(() => {
+        let nextIndex = (currentIndex + 1) % TOTAL_SLIDES;
+            goToSlide(nextIndex);
+        }, SLIDE_TIME);
+    }
+
+    function stopAutoSlide() {
+        if (interval !== null) {
+            clearInterval(interval);
+            interval = null;
+        }
+    }
+
+    BUTTONS.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = parseInt(button.dataset.index);
+            goToSlide(index);
+            stopAutoSlide();
+            startAutoSlide();
+        });
+    });
+
+    SLIDER.addEventListener('mouseenter', stopAutoSlide);
+    SLIDER.addEventListener('mouseleave', startAutoSlide);
+
+    // Başlat
+    goToSlide(0);
+    startAutoSlide();
+});
