69235aedf5ed48442546069d94f13dd11cba43ff
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-07-05 16:37:37 +0300

    Update utils.js
    1. Updated 'Dark/Light Mode' feature to localStorage.
    2. Updated 'Expand/Collapse Mode' feature to localStorage.
    3. Added code for 'modal'.

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+	loadDarkLightMode();
+
+	const STORAGE_KEY = getExpandCollapseLocalStorage('detailsState');
+	document.addEventListener('click', function(event) {
+		toggleExpandCollapse(event, 'detailsState', STORAGE_KEY);
+	});
+});
+
 /**
 * Switch Dark/Light Mode Function
 * -
+* ---
+* Save as localStorage
 */
 function toggleDarkLightMode() {
 	let element = document.body;
@@ -8,25 +19,87 @@ function toggleDarkLightMode() {
 
 	if (element.classList.contains('dark')) {
 		element.classList.remove('light');
-		setCookieSubKey('STv7ComTheme', 'darkMode', true);
+		localStorage.setItem('darkMode', 'true');
 	} else {
 		element.classList.add('light');
-		setCookieSubKey('STv7ComTheme', 'darkMode', false);
+		localStorage.setItem('darkMode', 'false');
 	}
 }
 
 /**
-* Save 'Expand/Collapse' status as cookie.
+* Load Dark/Light Mode Function
 * -
 * ---
-* @param {string} key cookie key
+* Check and apply dark/light mode.
 */
-function toggleExpandCollapse(key) {
-	const ELEMENT = document.getElementById(key);
-
-	if (ELEMENT.open) {
-		setCookieSubKey('STv6ComHide', key, 1);
-	} else {
-		setCookieSubKey('STv6ComHide', key, 0);
+function loadDarkLightMode() {
+	const IS_DARK_MODE = localStorage.getItem('darkMode');
+	if (IS_DARK_MODE === 'true') {
+		document.body.classList.add('dark');
 	}
+
+	document.getElementById("dark-light-mode").onclick = function() {
+		toggleDarkLightMode();
+		return false;
+	};
+}
+
+/**
+* getExpandCollapseLocalStorage Function
+* -
+* ---
+* Read localStorage data and return it as a json object.
+*
+* ---
+* @param {string} storageKey localStorage 'Key'
+* @return {object} localStorage obj
+*/
+function getExpandCollapseLocalStorage(storageKey) {
+	const SAVED_STATE = JSON.parse(localStorage.getItem(storageKey) || '{}');
+
+	Object.entries(SAVED_STATE).forEach(([id, state]) => {
+        const detailElement = document.getElementById(id);
+        if (detailElement) {
+            if (state === 'open') {
+                detailElement.setAttribute('open', '');
+            } else {
+                detailElement.removeAttribute('open');
+            }
+        }
+    });
+	return SAVED_STATE;
+}
+
+/**
+* Expand/Collapse Function
+* -
+* ---
+* @param {string} event EventListener 'click'
+* @param {string} storageKey localStorage 'Key'
+* @param {object} savedState localStorage obj
+*/
+function toggleExpandCollapse(event, storageKey, savedState) {
+	if (event.target.tagName === 'SUMMARY') {
+		const parentDetails = event.target.closest('details');
+
+		if (parentDetails && parentDetails.id) {
+			const detailId = parentDetails.id;
+			const isOpen = parentDetails.hasAttribute('open');
+
+			// Reverse the situation and save
+			savedState[detailId] = isOpen ? 'close' : 'open';
+			localStorage.setItem(storageKey, JSON.stringify(savedState));
+		}
+	}
+}
+
+/**
+* Modal Function
+* -
+* ---
+* @param {string} id modal element id.
+*/
+function getModal(id) {
+	document.body.classList.toggle('overflow-hidden');
+	document.getElementById(id).classList.toggle('invisible');
 }
